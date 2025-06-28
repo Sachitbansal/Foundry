46285fd80090f23216db045538decbc5868dfe62
Sachit Bansal <sachitbansal2006@gmail.com>
2025-06-29 02:01:43 +0530

    kal khatam kr dunga panchoooo fund me wala

--- FundMe/test/FundMeTest.t.sol
+++ FundMe/test/FundMeTest.t.sol
@@ -9,6 +9,11 @@ import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
+
+    address USER = makeAddr("user"); // this will create a new address for the user
+    uint256 constant SEND_VALUE = 0.1 ether; // this will be the amount we will send to the fundMe contract
+    uint256 constant STARTING_BALANCE = 10 ether; // this will be the starting balance of the user
+
     FundMe fundMe; // setting a storage/state variable to use throughout the contract
 
 
@@ -23,6 +28,7 @@ contract FundMeTest is Test{
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run(); // this will run the script and deploy the FundMe contract
 
+        vm.deal(USER, STARTING_BALANCE); // this will give the USER address 10 ETH
 
     }
 
@@ -40,10 +46,30 @@ contract FundMeTest is Test{
         // so we change msg.sender to address(this) to check if the address of the FundMeTest contract is the owner of the FundMe contract
         // we can use assertEq to check if the owner is set to the msg.sender
         assertEq(fundMe.i_owner(), msg.sender);
-        // this msg.sender is coming from deployFundMe script
+        // this msg.sender is coming from deployFundMe
     }
 
     function testPriceFeedVersionIsAccurate() public {
-        fundMe.ge
+        uint256 verison = fundMe.getVersion();
+        assertEq(verison, 4); // this is the version of the price feed we are using
+    }
+
+    // we are using foundry cheatcodes for writting this function
+    function testFundFailsWithoutEnoughEth() public {
+
+        vm.expectRevert(); // this will expect a revert
+        // means the assertion will fail if the function does not revert
+        // uint256 cat = 1; // this iwll not revert abd function will fail
+
+        fundMe.fund(); // this will revert because we are sending 0 ETH
+    }
+
+    function testFundUpdatesDataStructure() public {
+
+        vm.prank(USER); // this will make the next call to fundMe.fund() come from the USER address
+
+        fundMe.fund{value: SEND_VALUE}(); // this will send 10 ETH to the fundMe contract
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE); // this will check if the amount funded is equal to the SEND_VALUE
     }
 }
